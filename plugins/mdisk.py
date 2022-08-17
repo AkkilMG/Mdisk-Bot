79f71034ce283c7c271c8c3b499b4cc61dd6c468
AkKiL <78695802+HeimanPictures@users.noreply.github.com>
2022-08-17 18:26:34 +0530

    Update mdisk.py

--- plugins/mdisk.py
+++ plugins/mdisk.py
@@ -23,21 +23,28 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 async def remote_upload(bot, message):
     # await message.reply_chat_action("typing")
     
+    url = "https://diskuploader.mypowerdisk.com/v1/tp/cp"
     link = message.text #.split(' ')[0]
     if ('mypowerdisk.com/mybox' in link) or ('mdisk.me' in link):
         param = {'token':Config.API_KEY, 'link':str(link)} 
-        r = requests.post(link, json = param) 
-        response = r.json()
-        data = dict(response)
-        mdisk = data["sharelink"]
+        r = requests.post(url, json = param) 
         
-        url = mdisk
-        await message.reply_text(
-            text=f"**Video Link:** `{url}`\n\n This Code is made by @HeimanSupports.",
-            reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton('Doodstream', url=url)]]
+        if r.status_code == 200:
+            response = r.json()
+            data = dict(response)
+            mdisk = data["sharelink"]
+        
+            url = mdisk
+            await message.reply_text(
+                text=f"**Video Link:** `{url}`\n\n This Code is made by @HeimanSupports.",
+                reply_markup=InlineKeyboardMarkup(
+                    [[InlineKeyboardButton('Doodstream', url=url)]]
+                )
             )
-        )
+        else:
+            await message.reply_text(
+                "__I Am Facing Some Issue, Please Check the input link or its way that you have provided and if you feel its an error in code please contact **@HeimanSupport**__"
+            )
     else:
         await message.reply_text(
             "__I Am Facing Some Issue, Please Check the input link or its way that you have provided and if you feel its an error in code please contact **@HeimanSupport**__"
